1682268e1067059b69f4eb43a4c30c76c75e2b56
crispimUchoa <francisco,crispim@aluno.uece.br>
2025-02-04 19:43:36 -0300

    add x + dx/2 collumn

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def f(x):
 
 #a, b from integral
 a = 0 
-b = 2
+b = 3
 n = 20
 dx = (b - a)/n
 
@@ -21,24 +21,24 @@ def calculate_integral(f):
         x = a + i*dx
         trapezium_area = calculate_trapezium_area(x, f)
         total += trapezium_area
-        data = {'n': i, 'x': x, 'f(x + dx/2)': f(x + dx/2), 'dx*f(x + dx/2)': dx*f(x + dx/2)}
+        data = {'n': i, 'x': x,'x + dx/2':x + dx/2, 'f(x + dx/2)': f(x + dx/2), 'dx*f(x + dx/2)': dx*f(x + dx/2)}
         table_str.append(data)
     return total
 
-print('_'*85)
+print('_'*105)
 
-print(f'|{'n':20}|{'x':20}|{'f(x + dx/2)':20}|{'dx*f(x + dx/2)':20}|')
+print(f'|{'n':20}|{'x':20}|{'x + dx/2':20}|{'f(x + dx/2)':20}|{'dx*f(x + dx/2)':20}|')
 
-print('-'*85)
+print('-'*105)
 
 result = calculate_integral(f)
 
 for r in table_str:
 
-    print(f'|{r['n']:<20,.{rf}f}|{r['x']:<20,.{rf}f}|{r['f(x + dx/2)']:<20,.{rf}f}|{r['dx*f(x + dx/2)']:<20,.{rf}f}|')
+    print(f'|{r['n']:<20,.{rf}f}|{r['x']:<20,.{rf}f}|{r['x + dx/2']:<20,.{rf}f}|{r['f(x + dx/2)']:<20,.{rf}f}|{r['dx*f(x + dx/2)']:<20,.{rf}f}|')
 
 
-print('-'*85)
+print('-'*105)
 
 
 print(f'Σ = {result:.{rf}f}' )
